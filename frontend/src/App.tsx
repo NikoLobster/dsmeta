d6e39d30e355a90b478a4bd39b0bb23d0f0f6e2d
NikoLobster <joaopaulo.santos1378@gmail.com>
2022-11-15 13:46:49 -0300

    First component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,16 @@
+import NotificationButton from "./componentes/NotificationButton"
+
 function App() {
   return(
+    <>
     <h1>Olá, mundo</h1>
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    <NotificationButton />
+    </>
+    
   )
 }
 
